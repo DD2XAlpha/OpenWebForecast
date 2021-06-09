5a0a55e5ef3011b3c4f8e8013145b5892eb5909c
DD2XALPHA <danielalanis1997@outlook.com>
2021-06-09 15:03:59 -0500

    Add files via upload

---
+++ JS/config.js
@@ -0,0 +1,3 @@
+var config = {
+    MY_API_TOKEN : '1f98827dc50ba40c70719a6a8174dd3d'
+  }
---
+++ JS/main.js
@@ -0,0 +1,38 @@
+var audio;
+
+$(document).ready(function(){
+
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "http://api.openweathermap.org/data/2.5/weather?id=524901&lang=fr&appid=" + config.MY_API_TOKEN,
+        "method": "GET"
+      }
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+      });
+
+
+
+
+    $("#btnVolumen").on('click', function(){
+        PlaySound("sound/night.mp3")
+    })
+
+    $("#btnMute").on("click", function(){
+        audio.pause();
+        audio.currentTime = 0;
+    })
+
+    
+})
+
+function PlaySound(sound){
+    audio = new Audio(sound);
+    audio.autoplay = true;
+    audio.play();
+}
+
+function StopSound(){
+    audio.stop();
+}
